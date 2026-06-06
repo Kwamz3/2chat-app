888d1aa1308cd0f10ed9dab18f06df611f239feb
Kwamz3 <charlie18appiah@gmail.com>
2026-06-06 15:30:35 +0000

    feat: initialize http server and integrate socket.io setup in server.js

--- original/backend/server.js
+++ original/backend/server.js
@@ -1,11 +1,13 @@
 import http from "node:http";
 import app from "./app.js";
 import { setupSocket } from "./socket";
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 setupSocket(server);
 
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:5000`)
+  console.log(`Server is running on http://localhost:5000`);
 });
+
+export default server;
